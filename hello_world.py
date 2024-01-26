1a0db7d8b03f8e3da87c69d12a587dabb90edaee
Sage Phillips <sage242@hotmail.com>
2024-01-26 13:55:10 -0600

    Added some more lines to hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
+import time
 print("Hello world!")
+time.sleep(1)
+print("Seeing is believing. Follow me Neo.")
+i = 250
+time.sleep(1)
+while i > 0:
+	print("All work and no play makes Jack a dull boy. All work and no play makes Jack a dull boy. All work and no play makes Jack a dull boy. All work and no play makes Jack a dull boy.")
+	time.sleep(.1)
+	i -= 1
